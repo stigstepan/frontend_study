10f6a9e178ce0c5a7002950d0c2d254f464d1f01
Stigstepan <st.ig@mail.ru>
2018-10-08 17:08:41 +0300

    new algoritmths + help area

---
+++ backpack.js
@@ -0,0 +1,47 @@
+function backpack(data){
+    
+    let n = data.toString().split("\n"); 
+    let things = [];
+    let len = parseInt(n[0]); // number of things
+    let volume = parseFloat(n[0].split(' ')[1]);
+    let genV = 0;
+    let genC = 0;
+
+
+    for(let i = 1; i < len + 1; i++)
+    {
+        let nums = n[i].split(' ');
+        let n1 = parseFloat(nums[0]), n2 = parseFloat(nums[1]);
+        if(n2==0) continue;
+        things.push([n1, n2, n1/n2]);
+    }
+
+    things.sort(sortByCost);
+    
+    for (let i = 0; i < things.length; i++){
+        if(genV + things[i][1] <= volume){
+            genC += things[i][0];
+            genV += things[i][1];
+        } else {
+            let r = volume - genV;
+            if (r == 0) break;
+            things[i][0] *= r/things[1];
+            things[i][1] = r;
+            genC += things[i][0];
+            genV += things[i][1];
+            break;
+        }
+        
+    }
+
+    
+    let label_out = document.getElementById('data-out');
+    
+    label_out.value = genC.toFixed(3);
+    
+}
+
+function sortByCost(a,b){
+    if(a[2] < b[2]) return 1;
+    if(a[2] > b[2]) return -1;
+}
--- main.js
+++ main.js
@@ -3,6 +3,7 @@ start_button.addEventListener("click", start);
 var list = document.getElementById('list');
 list.addEventListener("change", select_item);
 var label_in = document.getElementById('data-in');
+var help = document.getElementById('help');
 // import {euclid} from "./euclid.js";
 // import {fibonach} from "./fibonach";
 
@@ -21,6 +22,9 @@ function start() {
         case "Segments":{
             segments(label_in.value);
         } break;
+        case "Backpack":{
+            backpack(label_in.value);
+        }
     }
 }
 
@@ -28,18 +32,19 @@ function select_item(){
     switch ( list.options[list.selectedIndex].value) {
         case "Fibonach":{
             label_in.value = "15";
+            help.value = "Дано целое число 1≤n≤30, необходимо вычислить n-е число Фибоначчи.";
         } break;
         case "Euclid":{
             label_in.value = "20 32";
+            help.value = "По данным двум числам 1≤a,b≤2⋅10^9 найдите их наибольший общий делитель.";
         }  break;
-<<<<<<< HEAD
         case "Segments":{
             label_in.value = "4\n4 7\n1 3\n2 5\n5 6"
-        }
-=======
-        case "Section":{
-            label_in.value = "???";
-        }  break;
->>>>>>> b23a2ae3531d1addfb3e20646dab811d137ad3d6
+            help.value = "По данным n отрезкам необходимо найти множество точек минимального размера, для которого каждый из отрезков содержит хотя бы одну из точек.\nВ первой строке дано число 1≤n≤100 отрезков. Каждая из последующих n строк содержит по два числа 0≤l≤r≤10^9, задающих начало и конец отрезка. Выведите оптимальное число m точек и сами m точек. Если таких множеств точек несколько, выведите любое из них.";
+        } break;
+        case "Backpack":{
+            label_in.value = "3 50\n60 20\n100 50\n120 30"
+            help.value = "Первая строка содержит количество предметов 1≤n≤10^3\nи вместимость рюкзака 0≤W≤2⋅10^6. \nКаждая из следующих n строк задаёт стоимость 0≤ci≤2⋅10^6\ и объём 0<wi≤2⋅10^6 предмета (n, W, ci, wi— целые числа). \nВыведите максимальную стоимость частей предметов (от каждого предмета можно отделить любую часть, стоимость и объём при этом пропорционально уменьшатся), помещающихся в данный рюкзак, с точностью не менее трёх знаков после запятой.";
+        }break;
     }
 }
