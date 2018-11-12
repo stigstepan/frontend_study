2ad6d349d40322c0b85391d94703fb7f55f36cf0
Stepan <32771848+stigstepan@users.noreply.github.com>
2018-11-12 19:51:47 +0300

    half of queue algorithm

--- queue.js
+++ queue.js
@@ -1,13 +1,19 @@
+const _top = 0;
+
 function queue(data) {
     try {
         let strs = data.toString().split("\n");
         let cnt = parseInt(strs[0]);
-        let que = [];
+
+
+        const heap = new PriorityQueue();
+
         // let's do commands
+        label_out.innerHTML = "";
         for (let i = 1; i < cnt + 1; i++) {
             let splt = strs[i].split(' ');
-            if (splt[1]) insert(que, splt[1]);
-            else extractMax(que);
+            if (splt[1]) heap.push(splt[1]);
+            else label_out.innerHTML += heap.pop() + '<br \/>';
         }
     } catch (e) {
         label_out.innerHTML = "Некорректный ввод.";
@@ -15,14 +21,80 @@ function queue(data) {
     }
 }
 
-function insert(que, num) {
-    for (let i = 0; i < que.lenght; i++) {
+// массив с приоритетами на основе кучи
 
+
+class PriorityQueue {
+    constructor(comparator = (a, b) => a > b) {
+        this._heap = [];
+        this._comparator = comparator;
     }
-
+    size() {
+        return this._heap.length;
+    }
+    isEmpty() {
+        return this.size() == 0;
+    }
+    peek() {
+        return this._heap[_top];
+    }
+    push(...values) {
+        values.forEach(value => {
+            this._heap.push(value);
+            this._siftUp();
+        });
+        return this.size();
+    }
+    pop() {
+        const poppedValue = this.peek();
+        const bottom = this.size() - 1;
+        if (bottom > _top) {
+            this._swap(_top, bottom);
+        }
+        this._heap.pop();
+        this._siftDown();
+        return poppedValue;
+    }
+    replace(value) {
+        const replacedValue = this.peek();
+        this._heap[_top] = value;
+        this._siftDown();
+        return replacedValue;
+    }
+    _greater(i, j) {
+        return this._comparator(this._heap[i], this._heap[j]);
+    }
+    _swap(i, j) {
+        [this._heap[i], this._heap[j]] = [this._heap[j], this._heap[i]];
+    }
+    _siftUp() {
+        let node = this.size() - 1;
+        while (node > _top && this._greater(node, parent(node))) {
+            this._swap(node, parent(node));
+            node = parent(node);
+        }
+    }
+    _siftDown() {
+        let node = _top;
+        while (
+            (left(node) < this.size() && this._greater(left(node), node)) ||
+            (right(node) < this.size() && this._greater(right(node), node))
+        ) {
+            let maxChild = (right(node) < this.size() && this._greater(right(node), left(node))) ? right(node) : left(node);
+            this._swap(node, maxChild);
+            node = maxChild;
+        }
+    }
 }
 
-function extractMax(que) {
-    let max = que.reverse().pop();
-        label_out.innerHTML += max + '<br \/>';
+function parent(i) {
+    return ((i + 1) >>> 1) - 1;
+}
+
+function left(i) {
+    return (i << 1) + 1;
+}
+
+function right(i) {
+    return (i + 1) << 1;
 }
